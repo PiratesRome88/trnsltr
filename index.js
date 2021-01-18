5afa55fb55038a2c46cdbdfff8cf2d3ba18e67b7
rchomko <romanmediapr@gmail.com>
2021-01-19 01:24:45 +0200

    server up!

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+require("dotenv").config();
+const colour = require("colour");
+const express = require("express");
+const http = require("http");
 
+const router = express();
+
+colour.setTheme({
+  silly: "rainbow",
+  input: "grey",
+  verbose: "cyan",
+  prompt: "grey",
+  info: "green",
+  success: "cyan bold",
+  data: "grey",
+  help: "cyan",
+  warn: ["yellow", "underline"],
+  debug: "blue",
+  error: "red bold",
+});
+
+let app = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello Roman!\n");
+});
+
+// Start the server on port 3000
+app.listen(3000, "127.0.0.1");
+console.log("Node server running on port 3000".success);
+
+router.get("/", function (req, res) {
+  res.send("hello roman");
+});
